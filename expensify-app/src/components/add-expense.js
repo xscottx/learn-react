decdfb1c5923cbfe01c29f5cf506148db3d8e834
Hoang Vo <hoangvo@gmail.com>
2018-01-03 23:00:51 -0600

    chore: started expense form

--- expensify-app/src/components/add-expense.js
+++ expensify-app/src/components/add-expense.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './expense-form';
 
 const AddExpensePage = () => (
   <div>
-    This is from my add expense page
+    <h1>Add Expense</h1>
+    <ExpenseForm />
   </div>
 )
 
