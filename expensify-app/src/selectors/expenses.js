3fc7bece1291c9b719ffac276955ba42224660cd
Hoang Vo <hoangvo@gmail.com>
2018-01-09 20:09:07 -0600

    feature: daterangepicker addition

--- expensify-app/src/selectors/expenses.js
+++ expensify-app/src/selectors/expenses.js
@@ -1,14 +1,15 @@
-// timestamps (milliseconds)
-// January 1st 1970 (unix epoch)
-// 33400, 10, -203
+import moment from 'moment';
+
 // Get visible expenses
 // JS toLowerCase() lowers strings, and .includes() checks if the text is inside string
 const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
 
   return expenses.filter((expense) => {
-    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;;
-    const textMatch = expense.description ? expense.description.toLowerCase().includes(text.toLowerCase()) : '';
+    // create a moment for expense and filter on dates before or after the start/end date match.
+    const createdAtMoment = moment(expense.createdAt);
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
   }).sort((a, b) => {
