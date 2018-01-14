84eeb8c7574a596076c6b69b35a84fa3b767aa5a
Hoang Vo <hoangvo@gmail.com>
2018-01-14 09:28:23 -0600

    feature: testing basic react components

--- expensify-app/src/components/expenses-list.js
+++ expensify-app/src/components/expenses-list.js
@@ -3,12 +3,19 @@ import {connect} from 'react-redux';
 import ExpenseListItem from './expense-list-item';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+// adding named export here to test dynamic component without connect
+export const ExpenseList = (props) => (
   <div>
-    <h1>Expense List</h1>
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem key={expense.description} {...expense} />
-    })}
+    {
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+        props.expenses.map((expense) => {
+          return <ExpenseListItem key={expense.id} {...expense} />
+        })
+      )
+    }
+    
   </div>
 )
 
--- expensify-app/src/tests/components/header.test.js
+++ expensify-app/src/tests/components/header.test.js
@@ -1,15 +1,22 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
 import Header from '../../components/header';
+// http://airbnb.io/enzyme/docs/api/
+import { shallow } from 'enzyme';
+// import toJSON from 'enzyme-to-json'; // don't need this anymore bc we have it running automatically under package.json 'test'
 
 // shallow rendering or DOM rendering
 // use shallow when not worried about lifecycle events
 // full DOM rendering renders child components
 
-// raf - request animatino frame
+// raf - request animation frame
 
 test('should render header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+
+  // Old school way of doing shallow rendering using dated lib, use Enzyme instead
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 })
