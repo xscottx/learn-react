113b3260e376d19ddf62a6e001f6be2207a6032b
Hoang Vo <hoangvo@gmail.com>
2018-01-23 22:58:37 -0600

    feature: added prod builds

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -5,36 +5,68 @@ const path = require('path');
 
 console.log(path.join(__dirname, 'public'));
 
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  console.log('env', env);
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    // LIFE HACK! source map shows exact file and line of code where error is in development
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  }
+}
 // node thing, allows node to see configs
 // can't just use JSX inside webpack without babel
 // created a rule for file that ends in .js not in node_modules, run thru babel
 // created new rule to handle css and scss (style) loaders
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  // LIFE HACK! source map shows exact file and line of code where error is in development
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-};
+// module.exports = {
+//   entry: './src/app.js',
+//   output: {
+//     path: path.join(__dirname, 'public'),
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [{
+//       loader: 'babel-loader',
+//       test: /\.js$/,
+//       exclude: /node_modules/
+//     }, {
+//       test: /\.s?css$/,
+//       use: [
+//         'style-loader',
+//         'css-loader',
+//         'sass-loader'
+//       ]
+//     }]
+//   },
+//   // LIFE HACK! source map shows exact file and line of code where error is in development
+//   devtool: 'cheap-module-eval-source-map',
+//   devServer: {
+//     contentBase: path.join(__dirname, 'public'),
+//     historyApiFallback: true
+//   }
+// };
 
 // loader
