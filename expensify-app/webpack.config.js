04bba35d7ae9d769ff8969450f7c258e16f76366
Hoang Vo <hoangvo@gmail.com>
2018-01-24 23:11:11 -0600

    feature: added run server w/ webpack bundle build

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -2,11 +2,14 @@
 
 // require is built-in node function
 const path = require('path');
+// required to extract css files into their own file
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 console.log(path.join(__dirname, 'public'));
 
 module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   console.log('env', env);
   return {
@@ -22,15 +25,29 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: { 
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
     },
+    plugins: [
+      CSSExtract
+    ],
     // LIFE HACK! source map shows exact file and line of code where error is in development
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
