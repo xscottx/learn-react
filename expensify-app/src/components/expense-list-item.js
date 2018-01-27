ea885676f11eda961b1387474123a7caa1d62136
Hoang Vo <hoangvo@gmail.com>
2018-01-26 23:42:05 -0600

    chore: roll back

--- expensify-app/src/components/expense-list-item.js
+++ expensify-app/src/components/expense-list-item.js
@@ -3,18 +3,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {removeExpense} from '../actions/expenses';
-import numeral from 'numeral';
-import moment from 'moment';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>
-      {numeral(amount / 100).format('$0,0.00')}
-      - 
-      {moment(createdAt).format('MMMM Do, YYYY')}</p>
+    <p>{amount} - {createdAt}</p>
   </div>
 )
 
