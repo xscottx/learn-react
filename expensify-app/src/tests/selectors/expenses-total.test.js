131f50955d3cf989c547c229165fdfa3c97aa0ab
Hoang Vo <hoangvo@gmail.com>
2018-01-27 09:44:25 -0600

    fix: finally fixed the damn issue w/ heroku!

---
+++ expensify-app/src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,14 @@
+import expenses from '../fixtures/expenses';
+import ExpensesTotal from '../../selectors/expenses-total';
+
+test('should return 0 if no expenses', () => {
+
+});
+
+test('should correctly add up a single expense', () => {
+
+});
+
+test('should correctly add up multiple expenses', () => {
+
+});
